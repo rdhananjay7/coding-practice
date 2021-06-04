5f998ff7b47cfa91c9e58459d8f808b1b7b9ea80
Dhananjay Argade <argadeda@amazon.com>
2021-06-04 19:56:40 +0530

    Fixed the initial max values of the variables to be minus infinity

--- kadanes_algorithm.py
+++ kadanes_algorithm.py
@@ -1,11 +1,10 @@
 def kadanes_algorithm(array):
     # O(n) time | O(1) space
 
-    max_so_far = 0
-    max_ending_here = 0
+    max_so_far = float("-inf")
+    max_ending_here = float("-inf")
 
     for idx in range(len(array)):
-
         max_ending_here = max(max_ending_here + array[idx], array[idx])
         max_so_far = max(max_so_far, max_ending_here)
 
@@ -13,5 +12,6 @@ def kadanes_algorithm(array):
 
 
 if __name__ == "__main__":
-    arr = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+    # arr = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+    arr = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7]
     print(kadanes_algorithm(arr))
